16eab8d7368bce0635d0537f3ba5f4ff07431796
Anurag Bakshi <anuragbakshi123@gmail.com>
2024-05-28 19:43:55 +0100

    partially type prbufnode

--- src/prbufnode.ts
+++ src/prbufnode.ts
@@ -10,74 +10,76 @@ import { BasicNode } from "./basic-node";
  * @extends {BasicNode}
  * @constructor
  */
-export const PrBufNode = function (id, type, value) {
-    this.val = { id, type, value };
-    this.children = [];
-    this.parent = null;
-};
-PrBufNode.prototype = new BasicNode();
-PrBufNode.prototype.constructor = PrBufNode;
-
-PrBufNode.prototype.id = function () {
-    return parseInt(this.val.id);
-};
-PrBufNode.prototype.type = function () {
-    return this.val.type.toString();
-};
-PrBufNode.prototype.value = function () {
-    return this.val.value;
-};
-
-/**
- * Compares the number of descendants with the value specified in the map element.
- * If all the children have not yet been added, we continue adding to this element.
- */
-PrBufNode.prototype.findLatestIncompleteNode = function () {
-    //if it's a branch (map) node ('m') and has room,
-    //or if it's the root (identified by having a null parent), which has no element limit,
-    //then return this node
-    if (
-        (this.val.type === "m" &&
-            this.val.value > this.getTotalDescendantCount()) ||
-        null === this.parent
-    ) {
-        return this;
-    } else {
-        return this.parent.findLatestIncompleteNode();
+export class PrBufNode extends BasicNode<{
+    id: number;
+    type: string;
+    value: string;
+}> {
+    constructor(id: string | number, type: string, value: string) {
+        super({ id: Number(id), type, value });
     }
-};
-/**
- * Parses the input URL 'data' protocol buffer parameter into a tree
- * @param {string} urlToParse
- * @returns {PrBufNode|null}
- */
-PrBufNode.create = function (urlToParse) {
-    let rootNode = null;
-    let re = /data=!([^?&]+)/;
-    let dataArray = urlToParse.match(re);
-    if (!dataArray || dataArray.length < 1) {
-        re = /mv:!([^?&]+)/;
-        dataArray = urlToParse.match(re);
+
+    id() {
+        return this.val?.id;
     }
-    if (dataArray && dataArray.length >= 1) {
-        rootNode = new PrBufNode();
-        let workingNode = rootNode;
-        //we iterate through each of the elements, creating a node for it, and
-        //deciding where to place it in the tree
-        let elemArray = dataArray[1].split("!");
-        for (let i = 0; i < elemArray.length; i++) {
-            const elemRe = /^([0-9]+)([a-z])(.+)$/;
-            const elemValsArray = elemArray[i].match(elemRe);
-            if (elemValsArray && elemValsArray.length > 3) {
-                const elemNode = new PrBufNode(
-                    parseInt(elemValsArray[1]),
-                    elemValsArray[2],
-                    elemValsArray[3]
-                );
-                workingNode.addChild(elemNode);
-                workingNode = elemNode.findLatestIncompleteNode();
-            }
+
+    type() {
+        return this.val?.type;
+    }
+
+    value() {
+        return this.val?.value;
+    }
+
+    /**
+     * Compares the number of descendants with the value specified in the map element.
+     * If all the children have not yet been added, we continue adding to this element.
+     */
+    findLatestIncompleteNode() {
+        //if it's a branch (map) node ('m') and has room,
+        //or if it's the root (identified by having a null parent), which has no element limit,
+        //then return this node
+        if (
+            (this.val?.type === "m" &&
+                this.val?.value > this.getTotalDescendantCount()) ||
+            undefined === this.parent
+        ) {
+            return this;
+        } else {
+            return this.parent.findLatestIncompleteNode();
         }
     }
-    return rootNode;
-};
+    /**
+     * Parses the input URL 'data' protocol buffer parameter into a tree
+     */
+    static create(urlToParse: string) {
+        let rootNode = null;
+        let re = /data=!([^?&]+)/;
+        let dataArray = urlToParse.match(re);
+        if (!dataArray || dataArray.length < 1) {
+            re = /mv:!([^?&]+)/;
+            dataArray = urlToParse.match(re);
+        }
+        if (dataArray && dataArray.length >= 1) {
+            rootNode = new PrBufNode();
+            let workingNode = rootNode;
+            //we iterate through each of the elements, creating a node for it, and
+            //deciding where to place it in the tree
+            let elemArray = dataArray[1].split("!");
+            for (let i = 0; i < elemArray.length; i++) {
+                const elemRe = /^([0-9]+)([a-z])(.+)$/;
+                const elemValsArray = elemArray[i].match(elemRe);
+                if (elemValsArray && elemValsArray.length > 3) {
+                    const elemNode = new PrBufNode(
+                        parseInt(elemValsArray[1]),
+                        elemValsArray[2],
+                        elemValsArray[3]
+                    );
+                    workingNode.addChild(elemNode);
+                    workingNode = elemNode.findLatestIncompleteNode();
+                }
+            }
+        }
+        return rootNode;
+    }
+}
